a78f2e7c9c01ab91719fd6ea97b26c78e594a887
Camiyah Goins <tokyonote9@gmail.com>
2020-04-23 23:48:15 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,8 +12,9 @@ end
 
 def add_element_to_end_of_array(array, element)
   
-numbers_array << 4
+array = ["a", "b", "c", ] << "d"
 
+puts array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
